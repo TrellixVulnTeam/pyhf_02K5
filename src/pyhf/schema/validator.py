f56d3dd9bfd9f992aade370452d85724b42bc034
Giordon Stark <kratsg@gmail.com>
2022-09-01 11:22:45 -0700

    update validator to latest draft

--- src/pyhf/schema/validator.py
+++ src/pyhf/schema/validator.py
@@ -27,11 +27,11 @@ def validate(spec: Mapping, schema_name: str, version: Union[str, None] = None):
 
     # note: trailing slash needed for RefResolver to resolve correctly
     resolver = jsonschema.RefResolver(
-        base_uri=f"file://{variables.schemas}/{version}/",
-        referrer=f"{schema_name}",
+        base_uri=f"file://{variables.schemas}/{version}/{schema_name}",
+        referrer={},
         store=variables.SCHEMA_CACHE,
     )
-    validator = jsonschema.Draft6Validator(
+    validator = jsonschema.Draft202012Validator(
         schema, resolver=resolver, format_checker=None
     )
 
