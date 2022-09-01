ec9e18f6cd88a1b5d281b1eb70a11a577186d0e6
Giordon Stark <kratsg@gmail.com>
2022-09-01 11:49:32 -0700

    add warning when version is wrong

--- src/pyhf/schema/validator.py
+++ src/pyhf/schema/validator.py
@@ -1,8 +1,11 @@
 import jsonschema
+import logging
 import pyhf.exceptions
 from pyhf.schema.loader import load_schema
 from pyhf.schema import variables
 from typing import Union, Mapping
+
+log = logging.getLogger(__name__)
 
 
 def validate(spec: Mapping, schema_name: str, version: Union[str, None] = None):
@@ -22,6 +25,10 @@ def validate(spec: Mapping, schema_name: str, version: Union[str, None] = None):
     """
 
     version = version or variables.SCHEMA_VERSION
+    if version != variables.SCHEMA_VERSION:
+        log.warning(
+            f"Specification requested version {version} but latest is {variables.SCHEMA_VERSION}. Upgrade your specification or downgrade pyhf."
+        )
 
     schema = load_schema(f'{version}/{schema_name}')
 
